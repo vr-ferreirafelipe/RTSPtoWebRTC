ebc2bc8fc01cf09cb2c56dec1ac5fe6d951e7f8e
Felipe Ferreira <ferreira.felipe@vrsoft.com.br>
2024-04-18 15:09:23 -0300

    vai que da bom

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,5 +1,5 @@
 let stream = new MediaStream();
-let suuid = $('#suuid').val();
+let suuid = "ecf-001"
 
 let config = {
   iceServers: [{
@@ -28,42 +28,97 @@ async function handleNegotiationNeededEvent() {
   getRemoteSdp();
 }
 
-$(document).ready(function() {
-  $('#' + suuid).addClass('active');
+// $(document).ready(function() {
+//   $('#' + suuid).addClass('active');
+//   getCodecInfo();
+// });
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById(suuid).classList.add('active');
   getCodecInfo();
 });
 
 
-function getCodecInfo() {
-  $.get("../codec/" + suuid, function(data) {
-    try {
-      data = JSON.parse(data);
-    } catch (e) {
-      console.log(e);
-    } finally {
-      $.each(data,function(index,value){
-        pc.addTransceiver(value.Type, {
-          'direction': 'sendrecv'
-        })
-      })
+
+// function getCodecInfo() {
+//   $.get("../codec/" + suuid, function(data) {
+//     try {
+//       data = JSON.parse(data);
+//     } catch (e) {
+//       console.log(e);
+//     } finally {
+//       $.each(data,function(index,value){
+//         pc.addTransceiver(value.Type, {
+//           'direction': 'sendrecv'
+//         })
+//       })
+//     }
+//   });
+// }
+
+async function getCodecInfo() {
+  try {
+    const response = await fetch("../codec/" + suuid);
+    if (!response.ok) {
+      throw new Error('Failed to fetch codec info');
     }
-  });
+    const data = await response.json();
+    data.forEach(function(value) {
+      pc.addTransceiver(value.Type, {
+        'direction': 'sendrecv'
+      });
+    });
+  } catch (error) {
+    console.error(error);
+  }
 }
+
 
 let sendChannel = null;
 
+// function getRemoteSdp() {
+//   $.post("../receiver/"+ suuid, {
+//     suuid: suuid,
+//     data: btoa(pc.localDescription.sdp)
+//   }, function(data) {
+//     try {
+//       pc.setRemoteDescription(new RTCSessionDescription({
+//         type: 'answer',
+//         sdp: atob(data)
+//       }))
+//     } catch (e) {
+//       console.warn(e);
+//     }
+//   });
+// }
+
+
 function getRemoteSdp() {
-  $.post("../receiver/"+ suuid, {
-    suuid: suuid,
-    data: btoa(pc.localDescription.sdp)
-  }, function(data) {
+  fetch("http://127.0.0.1:8083/stream/receiver/" + suuid, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Connection': 'keep-alive'
+    },
+    body: "suuid=" + encodeURIComponent(suuid) + "&data=" + encodeURIComponent(btoa(pc.localDescription.sdp))
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Erro ao fazer a requisição: ' + response.status);
+    }
+    return response.text();
+  })
+  .then(data => {
     try {
       pc.setRemoteDescription(new RTCSessionDescription({
         type: 'answer',
         sdp: atob(data)
-      }))
+      }));
     } catch (e) {
       console.warn(e);
     }
+  })
+  .catch(error => {
+    console.error(error);
   });
 }
